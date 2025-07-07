346de537a99dca842535dee1d487a4f2de38080a
zabackary <137591653+zabackary@users.noreply.github.com>
2025-07-07 16:50:51 -0500

    fix: minimize bundle size via fonts

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -5,3 +5,4 @@ declare module "@fontsource-variable/roboto-flex" {}
 declare module "@fontsource-variable/roboto-flex/full" {}
 declare module "@fontsource-variable/material-symbols-rounded" {}
 declare module "@fontsource-variable/material-symbols-rounded/full" {}
+declare module "@fontsource-variable/material-symbols-rounded/fill" {}
